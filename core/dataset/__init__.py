198025f593c2978013545aa8dd93f12f51ac97d3
may4m <codegeek77@gmail.com>
2017-04-04 12:36:29 +0200

    refactored

--- core/dataset/__init__.py
+++ core/dataset/__init__.py
@@ -1,2 +1,1 @@
-
-import dataset
+from dataset import *
--- core/dataset/dataset.py
+++ core/dataset/dataset.py
@@ -1,23 +1,41 @@
 
 from JsonAnalysisEngine.internal.factory import Factory
 
+import collections
 
-class DatasetBase(object):
+class DatasetBase(collections.Counter):
     pass
 
 
-class KeysDataset(DatasetBase, list):
+class KeysContainer(DatasetBase):
     pass
 
 
-class ValuesDataset(DatasetBase, list):
+class ValuesContainer(DatasetBase):
     pass
 
 
 class Dataset(object):
 
     def __init__(self):
-        self.keys_dataset = KeysDataset()
-        self.values_dataset = ValuesDataset()
+        self.keys_container = get_container(KeysContainer)
+        self.values_container = get_container(ValuesContainer)
+
+
+def MakeContainer(container_class, *args, **kwargs):
+    """
+    factory entry point for containers
+    """
+
+    if container_class == KeysContainer:
+        container = Factory.classes.get('KeysContainer')
+        if not container:
+            Factory.register('KeysContainer', cls=KeysContainer(*args, **kwargs))
+        return Factory.KeysContainer
+    elif container_class == ValuesContainer:
+        container = Factory.classes.get('ValuesContainer')
+        if not container:
+            Factory.register('ValuesContainer', cls=ValuesContainer(*args, **kwargs))
+        return Factory.ValuesContainer
 
 Factory.register('Dataset', cls=Dataset())
